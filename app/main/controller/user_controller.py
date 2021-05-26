45af0fa4bd5f2cfe0a4b6967a9163aaad633a90a
seojinsarejj <s19009@naver.com>
2021-05-26 12:05:31 +0900

    [CREATE] Create token required

--- app/main/controller/user_controller.py
+++ app/main/controller/user_controller.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask_restplus import Resource
 
+from app.main.utils.decorator import token_required
 from ..utils.dto import UserDto
 from ..service.user_service import save_new_user, get_all_users, get_a_user
 
@@ -10,12 +11,14 @@ _user = UserDto.user
 
 @api.route("/")
 class UserList(Resource):
+    @token_required
     @api.doc("list_of_registered_users")
     @api.marshal_list_with(_user, envelope="data")
     def get(self):
         """List all registered users"""
         return get_all_users()
 
+    @token_required
     @api.response(201, "User successfully created.")
     @api.doc("create a new user")
     @api.expect(_user, validate=True)
@@ -29,6 +32,7 @@ class UserList(Resource):
 @api.param("public_id", "The User identifier")
 @api.response(404, "User not found.")
 class User(Resource):
+    @token_required
     @api.doc("get a user")
     @api.marshal_with(_user)
     def get(self, public_id):
