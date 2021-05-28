aa1215dd53fbdb09e3b892d025af5f97e8e6037b
seojinsarejj <s19009@naver.com>
2021-05-28 13:11:58 +0900

    [ADD] Add ondelete cascade

--- app/main/model/gallery.py
+++ app/main/model/gallery.py
@@ -8,10 +8,12 @@ class Gallery(db.Model):
     __tablename__ = "gallery"
 
     gallery_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id, ondelete="CASCADE"))
     name = db.Column(db.String(50))
     description = db.Column(db.Text)
     created_at = db.Column(db.DateTime, nullable=False)
+
+    user = db.relationship("User", backref=db.backref("gallery_set", cascade="delete"))
 
     def __init__(self, user_id, name, description):
         self.user_id = user_id
@@ -28,11 +30,18 @@ class Comment(db.Model):
 
     comment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     gallery_id = db.Column(
-        db.Integer, db.ForeignKey(Gallery.gallery_id), nullable=False
+        db.Integer,
+        db.ForeignKey(Gallery.gallery_id, ondelete="CASCADE"),
+        nullable=False,
     )
-    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id, ondelete="CASCADE"))
     comment = db.Column(db.Text)
     created_at = db.Column(db.DateTime, nullable=False)
+
+    gallery = db.relationship(
+        "Gallery", backref=db.backref("comment_set", cascade="delete")
+    )
+    user = db.relationship("User", backref=db.backref("comment_set", cascade="delete"))
 
     def __init__(self, gallery_id, user_id, comment):
         self.gallery_id = gallery_id
@@ -49,10 +58,17 @@ class Likes(db.Model):
 
     likes_id = db.Column(db.Integer, primary_key=True)
     gallery_id = db.Column(
-        db.Integer, db.ForeignKey(Gallery.gallery_id), nullable=False
+        db.Integer,
+        db.ForeignKey(Gallery.gallery_id, ondelete="CASCADE"),
+        nullable=False,
     )
-    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id, ondelete="CASCADE"))
     created_at = db.Column(db.DateTime, nullable=False)
+
+    gallery = db.relationship(
+        "Gallery", backref=db.backref("likes_set", cascade="delete")
+    )
+    user = db.relationship("User", backref=db.backref("user_set", cascade="delete"))
 
     def __init__(self, gallery_id, user_id):
         self.gallery_id = gallery_id
@@ -68,10 +84,17 @@ class LinkedGalleryPicture(db.Model):
 
     linked_id = db.Column(db.Integer, primary_key=True)
     gallery_id = db.Column(
-        db.Integer, db.ForeignKey(Gallery.gallery_id), nullable=False
+        db.Integer, db.ForeignKey(Gallery.gallery_id, ondelete="CASCADE")
     )
     picture_id = db.Column(
-        db.Integer, db.ForeignKey(Picture.picture_id), nullable=False
+        db.Integer, db.ForeignKey(Picture.picture_id, ondelete="CASCADE")
+    )
+
+    gallery = db.relationship(
+        "Gallery", backref=db.backref("linkedgallerypicture_set", cascade="delete")
+    )
+    picture = db.relationship(
+        "Picture", backref=db.backref("linkedgallerypicture_set", cascade="delete")
     )
 
     def __init__(self, gallery_id, picture_id):
--- app/main/model/picture.py
+++ app/main/model/picture.py
@@ -7,11 +7,13 @@ class Picture(db.Model):
     __tablename__ = "picture"
 
     picture_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id, ondelete="CASCADE"))
     image = db.Column(db.String(100), unique=True)
     filename = db.Column(db.String(100), unique=True)
     description = db.Column(db.Text)
     created_at = db.Column(db.DateTime, nullable=False)
+
+    user = db.relationship("User", backref=db.backref("picture_set", cascade="delete"))
 
     def __init__(self, user_id, image, filename, description):
         self.user_id = user_id
